1259a0f1611b39d486a0cd98c20fc38ebfaa719b
Lazar Karic <16634314+lkaric@users.noreply.github.com>
2024-06-08 17:57:43 +0200

    feat: :fire: cleaning up storybook

---
+++ .storybook/theme.ts
@@ -0,0 +1,14 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  brandTitle: "Perun",
+  brandUrl: "https://example.com",
+  brandImage:
+    "https://lh3.google.com/u/0/d/1k2VQEEZHbvFtEN0z1AqtMlNYqv4xNDkz=w2980-h1418-iv1",
+  brandTarget: "_self",
+});
